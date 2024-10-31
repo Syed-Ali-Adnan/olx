8fd4fda3b8a9dfc73af082235b74450d749ebfa3
Syed-Ali-Adnan <devsyedaliadnan@gmail.com>
2024-10-31 14:27:04 +0500

    adding

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,13 @@ const port = 5000
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["https://olx-syed-ali-adnans-projects.vercel.app"],
+    methods:["post","get"],
+    credentials:true
+  }
+));
 
 app.use(routes);
 
